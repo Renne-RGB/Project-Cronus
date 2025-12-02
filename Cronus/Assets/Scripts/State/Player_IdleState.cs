9f27cbea077ada399d5ede959f709bf979e2d51c
Renne-RGB <izumiding@gmail.com>
2025-12-03 02:28:00 +0900

    StateMachineとInputシステム

---
+++ Cronus/Assets/Scripts/State/Player_IdleState.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Player_IdleState : EntityState
+{
+    public Player_IdleState(Player player, StateMachine stateMachine, string animBoolName) : base(player, stateMachine, animBoolName)
+    {
+        
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if(player.moveInput.x != 0)
+            stateMachine.ChangeState(player.moveState);
+    }
+
+}
---
+++ Cronus/Assets/Scripts/State/Player_MoveState.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Player_MoveState : EntityState
+{
+    public Player_MoveState(Player player, StateMachine stateMachine, string animBoolName) : base(player, stateMachine, animBoolName)
+    {
+        
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if(player.moveInput.x == 0)
+            stateMachine.ChangeState(player.idleState);
+    }
+}
