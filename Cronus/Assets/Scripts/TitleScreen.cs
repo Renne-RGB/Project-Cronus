6fa6f13b146e461a7b3972eef1c1281ed82fcf7a
24cu0210-cpu <24cu0210@jec.ac.jp>
2026-01-09 10:40:11 +0900

    titlescreen

--- Cronus/Assets/Scripts/TitleScreen.cs
+++ Cronus/Assets/Scripts/TitleScreen.cs
@@ -3,26 +3,29 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEngine.Splines;
 
 public class TitleScreen : MonoBehaviour
 {
     public List<TextMeshProUGUI> textList;      // 選択項目。現在はテキストを入れてある
 
     int selectNum = 0;                          // 現在選択中の番号
-    int selectMin = 0;                          // 
-    int selectMax = 2;                          // 
+    int selectMax = 2;                          // 選択項目の最大数
     bool isSelected = false;                    // 選択されたかどうか
+    float timer = 0.0f;                         // 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        TextChangeColor();
+        ChangeTextColor();
     }
 
     void Update()
     {
         GetUp();
         GetDown();
+        GetEnter();
+        Select();
     }
 
     // メニュー選択　上方向
@@ -30,7 +33,7 @@ public class TitleScreen : MonoBehaviour
     {
         if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && !isSelected)
         {
-            if (selectNum == selectMin)
+            if (selectNum == 0)
             {
                 selectNum = selectMax;
             }
@@ -39,7 +42,7 @@ public class TitleScreen : MonoBehaviour
                 selectNum--;
             }
 
-            TextChangeColor();
+            ChangeTextColor();
         }
     }
 
@@ -50,20 +53,29 @@ public class TitleScreen : MonoBehaviour
         {
             if (selectNum == selectMax)
             {
-                selectNum = selectMin;
+                selectNum = 0;
             }
             else
             {
                 selectNum++;
             }
 
-            TextChangeColor();
+            ChangeTextColor();
         }
     }
 
+    // メニュー選択　エンターキー
+    void GetEnter()
+    {
+        if(Input.GetKeyDown(KeyCode.Return) && !isSelected)
+        {
+            isSelected = true;
+
+        }
+    }
 
     // 選択項目の色を変える
-    void TextChangeColor()
+    void ChangeTextColor()
     {
         for (int i = 0; i < textList.Count; i++)
         {
@@ -78,5 +90,44 @@ public class TitleScreen : MonoBehaviour
 
         }
     }
+
+    // 選択する処理
+    void Select()
+    {
+        if (isSelected)
+        {
+            timer += (Time.deltaTime * 3);
+
+            if (((int)timer % 2) == 0)
+            {
+                textList[selectNum].color = new Color(1.0f, 0.0f, 0.0f, 0.0f);
+            }
+            else
+            {
+                textList[selectNum].color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+            }
+
+            if (timer >= 3.0f)
+            {
+                isSelected = false;
+                timer = 0.0f;
+
+                if(selectNum == 0)
+                {
+                    SceneManager.LoadScene("Scenes/VisibleTest");
+                }
+                else if (selectNum == 1)
+                {
+                    SceneManager.LoadScene("Scenes/GameScene");
+                }
+                else if (selectNum == 2)
+                {
+                    Application.Quit();
+                }
+            }
+        }
+    }
+
+
 
 }
